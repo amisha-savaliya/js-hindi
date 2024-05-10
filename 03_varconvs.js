cff396e3fe5a450ff8fe43c08d1e47f8cf3c7a14
amisha-savaliya <169155224+amisha-savaliya@users.noreply.github.com>
2024-05-10 04:50:50 +0000

    conversion

---
+++ 03_varconvs.js
@@ -0,0 +1,27 @@
+let score="897"
+//let score=undefined
+//let score=true
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(score));
+console.log(valueInNumber);
+
+
+//"33"=>33
+//"33aaa"=> NaN
+
+
+let isloggedin=1
+let booleanIsloogedin =Boolean(isloggedin)
+console.log(booleanIsloogedin);
+
+//true=>1;false=>0
+//""=> false
+//"ami"=>true
+
+let sum=33
+
+let stringinnumber=String(sum)
+console.log(stringinnumber);
+console.log(typeof(stringinnumber));
